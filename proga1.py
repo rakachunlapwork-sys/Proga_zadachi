4eb8e1119c268d16dc2432d8f3436effca235e4f
rakachunlapwork-sys <rakachunlapwork@gmail.com>
2025-09-24 18:25:16 +0500

    1 programm

--- proga1.py
+++ proga1.py
@@ -1,1 +1,17 @@
-print("hello")
+number = int(input('Введите ваше число: '))
+if number % 2 ==0:
+    print('Число четное')
+else:
+    print('Число нечетное')
+if number > 10 and number < 50:
+    print('Число принадлежит диапазону')
+else:
+    print('Не принадлежит диапазону')
+if number>0:
+    print("число больше нуля")
+elif number<0:
+    print("число меньше нуля")
+elif number==0:
+    print("число равно нуля")
+else:
+    print("некорректное число")
