3869f63fc28ad40ec7d7585c1926a0f96809b4b6
rakachunlapwork-sys <rakachunlapwork@gmail.com>
2025-09-24 18:41:40 +0500

    +length

--- proga2.py
+++ proga2.py
@@ -2,14 +2,18 @@ import random # работа со случайными числами
 import string # за работу со строками
 spec_symbols = '!@#$%^&*('
 
-letters = ''.join(random.choice(string.ascii_uppercase) for _ in range(3))
+l1 = int(input("letters length: "))
+l2 = int(input("numbers length: "))
+l3 = int(input("symbols length: "))
+
+letters = ''.join(random.choice(string.ascii_uppercase) for _ in range(l1))
 
 #  3 случайные цифры
-numbers = ''.join(random.choice(string.digits) for _ in range(3))
+numbers = ''.join(random.choice(string.digits) for _ in range(l2))
 
 #  2 специальных символа
 
-symbols = ''.join(random.choice(spec_symbols) for _ in range(3))
+symbols = ''.join(random.choice(spec_symbols) for _ in range(l3))
 
 word = letters+numbers+symbols
 a = []
