147073373d75bfceed8ec0f7eb3583e0bfb22d7a
rakachunlapwork-sys <rakachunlapwork@gmail.com>
2025-10-21 19:56:36 +0500

    fride is finally done

--- Практика_4/Holodilnik.py
+++ Практика_4/Holodilnik.py
@@ -1,28 +1,34 @@
 # Fridge simulator
 
-def add(name, count, date=None):
-    items = {}
-    if name not in items:
-        items[name] = [{"amount" : count, 'expiration_date' : date}]
+def add(goods, name, count, date=None):
+    if name not in goods:
+        goods[name] = [{"amount" : count, 'expiration_date' : date}]
     else:
         name+="_batch_2"
-        items[name] = [{"amount" : count, 'expiration_date' : date}]
-    return items[name]
+        goods[name] = [{"amount" : count, 'expiration_date' : date}]
+    return goods[name]
 
+def amount(goods, product):
+    if product in goods:
+        return goods[product][0]["amount"]
+    else:
+        return "Продукт не найден!"
+    
 def main():
     goods = {}
     while True:
         print("Добро пожаловать в главное меню симулятора холодильника!")
         print("Чтобы посмотреть содержимое холодильника, введите 1")
         print("Чтобы добавить продукт в холодильник, введите 2")
-        print("Чтобы выйти из холодильника, введите 3")
+        print("Чтобы количество продукта по его названию, введите 3")
+        print("Чтобы выйти из холодильника, введите 4")
         while True:
             try:
                 n = int(input("Введите команду: "))
-                if n in [1, 2, 3]:
+                if n in [1, 2, 3, 4]:
                     break
             except:
-                print("Введите число от 1 до 3 !")
+                print("Введите число от 1 до 4 !")
 
         if n==1:
             print(goods)
@@ -64,11 +70,14 @@ def main():
             
             if year!=0 and day!=0 and month!=0:
                 date = str(day)+"-"+str(month)+"-"+str(year)
-                goods[name] = add(name, count, date)
+                goods[name] = add(goods, name, count, date)
             else:
-                goods[name] = add(name, count)
+                goods[name] = add(goods, name, count)
             print(f"Добавлен новый продукт '{name}'")
         elif n==3:
+            product = input("Введите название продукта : ")
+            print(f" вот количество продукта {product} : {amount(goods, product)}")
+        elif n==4:
             break
 
 main()
